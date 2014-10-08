b11a51b4c2b240022af384732aa44ed63b6c7133
melarish <melarish@gmail.com>
2014-10-08 17:52:36 +0100

    Oops forgot to save file

--- Cybernated_Farm.ino
+++ Cybernated_Farm.ino
@@ -1,15 +1,15 @@
-/*
-  AnalogReadSerial
-  Reads an analog input on pin 0, prints the result to the serial monitor.
-  Attach the center pin of a potentiometer to pin A0, and the outside pins to +5V and ground.
+#include <Servo.h> 
  
- This example code is in the public domain.
- */
+Servo myservo;  // create servo object to control a servo 
+                // twelve servo objects can be created on most boards
+ 
+int pos = 0;    // variable to store the servo position 
 
 // the setup routine runs once when you press reset:
 void setup() {
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object 
 }
 
 // the loop routine runs over and over again forever:
@@ -18,40 +18,24 @@ void loop() {
   int sensorValue = analogRead(A0);
   // print out the value you read:
   Serial.println(sensorValue);
-  delay(1);        // delay in between reads for stability
-}
+  delay(1000);        // delay in between reads for stability
 
-/* Sweep
- by BARRAGAN <http://barraganstudio.com> 
- This example code is in the public domain.
-
- modified 8 Nov 2013
- by Scott Fitzgerald
- http://arduino.cc/en/Tutorial/Sweep
-*/ 
-
-#include <Servo.h> 
- 
-Servo myservo;  // create servo object to control a servo 
-                // twelve servo objects can be created on most boards
- 
-int pos = 0;    // variable to store the servo position 
- 
-void setup() 
-{ 
-  myservo.attach(9);  // attaches the servo on pin 9 to the servo object 
-} 
- 
-void loop() 
-{ 
-  for(pos = 0; pos <= 180; pos += 10) // goes from 0 degrees to 180 degrees 
+  if (sensorValue < 300) // goes from 0 degrees to 180 degrees 
   {                                  // in steps of 1 degree 
+    pos = 180;
     myservo.write(pos);              // tell servo to go to position in variable 'pos' 
     delay(150);                       // waits 15ms for the servo to reach the position 
   } 
-  for(pos = 180; pos>=0; pos-=10)     // goes from 180 degrees to 0 degrees 
-  {                                
+  else if (sensorValue >= 300 && sensorValue < 600)
+  {
+    pos = 90;
     myservo.write(pos);              // tell servo to go to position in variable 'pos' 
-    delay(150);                       // waits 15ms for the servo to reach the position 
-  } 
+    delay(150); 
+  }
+  else
+  {
+    pos = 0;
+    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
+    delay(150); 
+  }
 } 
