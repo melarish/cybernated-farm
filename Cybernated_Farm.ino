d4fda6d522f493e94ba0008c01feb73bc3d639e8
Dave Walker <dave@thelemonpress.co.uk>
2014-10-08 18:06:48 +0100

    Better comments :p

--- Cybernated_Farm.ino
+++ Cybernated_Farm.ino
@@ -1,41 +1,41 @@
-#include <Servo.h> 
- 
-Servo myservo;  // create servo object to control a servo 
-                // twelve servo objects can be created on most boards
- 
-int pos = 0;    // variable to store the servo position 
+#include <Servo.h>
 
-// the setup routine runs once when you press reset:
+Servo myservo;  // create servo object to control a servo
+
+
+int pos = 0;    // variable to store the servo position
+
+
 void setup() {
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
-  myservo.attach(9);  // attaches the servo on pin 9 to the servo object 
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
-  // read the input on analog pin 0:
+  // read the moisture input on analog pin 0:
   int sensorValue = analogRead(A0);
   // print out the value you read:
   Serial.println(sensorValue);
   delay(1000);        // delay in between reads for stability
 
-  if (sensorValue < 300) // goes from 0 degrees to 180 degrees 
-  {                                  // in steps of 1 degree 
+  if (sensorValue < 300) // Soil is dry
+  {                                  // in steps of 1 degree
     pos = 180;
-    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
-    delay(150);                       // waits 15ms for the servo to reach the position 
-  } 
-  else if (sensorValue >= 300 && sensorValue < 600)
+    myservo.write(pos);              // tell servo to go to position 180
+    delay(150);                       // waits 150ms for the servo to reach the position
+  }
+  else if (sensorValue >= 300 && sensorValue < 600)  // Soil is moist
   {
     pos = 90;
-    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
-    delay(150); 
+    myservo.write(pos);              // tell servo to go to position 90
+    delay(150);
   }
-  else
+  else  // Soil is wet
   {
     pos = 0;
-    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
-    delay(150); 
+    myservo.write(pos);              // tell servo to go to position 0
+    delay(150);
   }
-} 
+}
